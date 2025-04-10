341f329a83412888394c1b9839f6f434250dbb7e
Rajatsalar <147129138+Rajatsalar@users.noreply.github.com>
2025-04-10 14:32:05 +0530

    Update AboutMe.jsx

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -19,16 +19,10 @@ const About = () => {
             <div className="about-text">
               <h3>I'm Rajat, a Frontend Developer</h3>
               <p>
-                Passionate frontend developer with experience in building
-                responsive and interactive web applications. I specialize in
-                <strong> React.js, Bootstrap, and JavaScript</strong>,
-                creating visually appealing and user-friendly interfaces.
+                With 2 years of hands-on experience at Zoruk EV Pvt. Ltd., I have played a key role in maintaining and developing their flagship website, zorukev.com. My work there sharpened my skills in web technologies and front-end development, where I transformed complex design concepts into user-friendly digital experiences. This role also allowed me to perfect my abilities in creating responsive layouts, optimizing user interfaces, and ensuring seamless performance across various devices, leveraging technologies such as <strong>React.js, Bootstrap, and JavaScript</strong>.
               </p>
               <p>
-                During my <strong>6-month internship</strong>, I worked on various
-                projects, optimizing website performance and enhancing UI/UX design. 
-                I am committed to continuous learning and enjoy experimenting with 
-                new technologies.
+               During my <strong>4-month internship at Avian We</strong>, I further expanded my expertise as a front-end developer by working on a range of live projects. I specialized in building custom WordPress themes and enhancing website functionality through innovative design and agile development practices. This experience enriched my problem-solving skills and deepened my understanding of current web trends, empowering me to deliver modern, high-performance websites tailored to client needs.
               </p>
 
               {/* Skills Section */}
