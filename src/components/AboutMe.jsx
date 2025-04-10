370c0c190ad2c6bbd4d7c255fac40495dd3f7ca1
Rajatsalar <147129138+Rajatsalar@users.noreply.github.com>
2025-04-10 15:06:50 +0530

    Update AboutMe.jsx

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -16,12 +16,12 @@ const About = () => {
         {/* Text & Skills Container */}
         <div className="about-text">
           <h3>I'm Rajat, a Frontend Developer</h3>
+           <p>
+                With <strong>2 years of hands-on experience at Zoruk EV Pvt. Ltd.</strong>, I have played a key role in maintaining and developing their flagship website, <a href="https://zorukev.com">zorukev.com</a>. My work there sharpened my skills in web technologies and front-end development, where I transformed complex design concepts into user-friendly digital experiences. This role also allowed me to perfect my abilities in creating responsive layouts, optimizing user interfaces, and ensuring seamless performance across various devices, leveraging technologies such as <strong>React.js, Bootstrap, and JavaScript</strong>.
+              </p>
           <p>
-            With <strong>2 years of hands-on experience at Zoruk EV Pvt. Ltd.</strong>, I have played a key role in maintaining and developing their flagship website, <a href="https://zorukev.com">zorukev.com</a>. My work sharpened my skills in web technologies and front-end development—transforming complex design concepts into user-friendly digital experiences while leveraging <strong>React.js, Bootstrap, and JavaScript</strong>.
-          </p>
-          <p>
-            During my <strong>4-month internship at Avian We</strong>, I further honed my expertise as a front-end developer by working on multiple live projects. I specialized in custom WordPress theme development and enhancing website functionality with innovative design and agile practices, ultimately deepening my understanding of current web trends.
-          </p>
+               During my <strong>4-month internship at Avian We</strong>, I further expanded my expertise as a front-end developer by working on a range of live projects. I specialized in building custom WordPress themes and enhancing website functionality through innovative design and agile development practices. This experience enriched my problem-solving skills and deepened my understanding of current web trends, empowering me to deliver modern, high-performance websites tailored to client needs.
+              </p>
           <div className="about-skills">
             <h4>Skills & Expertise</h4>
             <div className="skills-container">
